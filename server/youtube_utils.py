13f155840fde8f8a2ce60843f64a0d18919873e5
Jean A. Eckelberg <fyrelynx11@gmail.com>
2021-09-07 00:47:04 -0500

    Update youtube_utils.py

--- server/youtube_utils.py
+++ server/youtube_utils.py
@@ -2,6 +2,8 @@
 from apiclient.discovery import build
 from apiclient.errors import HttpError
 from oauth2client.tools import argparser
+from io import *
+import json
 
 DEVELOPER_KEY = "AIzaSyBuyUD1YN9SQ0mjjcyrH8q9vzXth0g67ok"
 YOUTUBE_API_SERVICE_NAME = "youtube"
@@ -12,33 +14,61 @@ def search(query):
     youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
     developerKey=DEVELOPER_KEY)
 
-
-    search_response = youtube.search().list(
-        q=query,
-        part="id,snippet",
-        maxResults=50
-    ).execute()
-
     videos = []
     channels = []
     playlists = []
+    saved_video = False;
+    
+    # Get related json file
+    with open(query[0].upper()+'.json', 'r') as read_file:
+        data = json.load(read_file)
+        
+    # Chack query against data
+    for item in data:
+        if query.upper() == item['query']:
+            saved_video = True;
+            videos.append(item)
+    
+    
+    # Check to see if the query passed is a direct link
+    if 'https://www.youtube.com/watch?v=' in query:
+        #Get unique video id from link
+        video_id = query.split('=')[1]
+        
+        videos.append({"title": "Direct Url Request",
+                "id": video_id,
+                "uploader": "Direct Url Request",
+                "description": "Direct Url Request",
+                "thumbnail": "https://static.wikia.nocookie.net/pokemon/images/d/d6/Jessie_Mimikyu.png/revision/latest?cb=20170915045921",
+                "url": query,
+                "autoplay_url": query + "?autoplay=1"})
+        })
+    
+    elif saved_video == True:
+        pass
+    else:
+        search_response = youtube.search().list(
+            q=query,
+            part="id,snippet",
+            maxResults=50
+        ).execute()
 
-    # Add each result to the appropriate list, and then display the lists of
-    # matching videos, channels, and playlists.
-    for search_result in search_response.get("items", []):
-        if search_result["id"]["kind"] == "youtube#video":
-            videos.append({
-                "title": search_result["snippet"]["title"],
-                "id": search_result["id"]["videoId"],
-                "uploader": search_result["snippet"]["channelTitle"],
-                "description": search_result["snippet"]["description"],
-                "thumbnail": search_result["snippet"]["thumbnails"]["high"],
-                "url": "https://www.youtube.com/embed/" + search_result["id"]["videoId"],
-                "autoplay_url": "https://www.youtube.com/embed/" + search_result["id"]["videoId"] + "?autoplay=1"
+        # Add each result to the appropriate list, and then display the lists of
+        # matching videos, channels, and playlists.
+        for search_result in search_response.get("items", []):
+            if search_result["id"]["kind"] == "youtube#video":
+                videos.append({
+                    "title": search_result["snippet"]["title"],
+                    "id": search_result["id"]["videoId"],
+                    "uploader": search_result["snippet"]["channelTitle"],
+                    "description": search_result["snippet"]["description"],
+                    "thumbnail": search_result["snippet"]["thumbnails"]["high"],
+                    "url": "https://www.youtube.com/embed/" + search_result["id"]["videoId"],
+                    "autoplay_url": "https://www.youtube.com/embed/" + search_result["id"]["videoId"] + "?autoplay=1"
 
-            })
+                })
 
-    return videos
+    return videos, query
 
 
 def search_related_videos(video_id):
@@ -76,7 +106,7 @@ def search_related_videos(video_id):
 
 
 
-def get_video(yt_id):
+def get_video(yt_id, query):
 
     youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
     developerKey=DEVELOPER_KEY)
@@ -94,6 +124,7 @@ def get_video(yt_id):
         return None
 
     video = {
+        "query": query.upper()
         "title": response["snippet"]["title"],
         "id": response["id"],
         "uploader": response["snippet"]["channelTitle"],
@@ -103,7 +134,10 @@ def get_video(yt_id):
         "autoplay_url": "https://www.youtube.com/embed/" + response["id"] + "?autoplay=1",
         "duration": response["contentDetails"]["duration"]
     }
-
+    
+    #TODO: Implement save video to json here
+    with open(query[0].upper()+'.json', 'w+') as data_file:
+        json.dump(video)
     #print(video)
 
     return video
